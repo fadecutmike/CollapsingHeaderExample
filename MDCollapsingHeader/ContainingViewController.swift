d79d7b004ac37d67f3d5b5089061199cce7f9113
Michael Dimore <michael.dimore@williamhill.us>
2021-11-30 04:28:41 -0500

    New and better collapsible header foundation

--- MDCollapsingHeader/ContainingViewController.swift
+++ MDCollapsingHeader/ContainingViewController.swift
@@ -16,36 +16,47 @@ class ContainingViewController: UIViewController {
     @IBOutlet weak var headerView: UIView!
     
     /// Constraint between the top of headerView and the top of the screen
-    @IBOutlet weak var headerViewTop: NSLayoutConstraint!
-
-    /// The minimum height of the header (fully collapsed header)
-    let minHeight: CGFloat = 100.0
+    @IBOutlet weak var headerTopLiftConstraint: NSLayoutConstraint!
+        
+    /// The CGFloat value that controls the top elevator lift constraint of the HeaderView
+    @IBOutlet weak var fullyCollapsedHeightConstraint: NSLayoutConstraint!
+    private(set) var fullyCollapsedHeightVal: CGFloat = 0.0 {
+        didSet { fullyCollapsedHeightConstraint.constant = fullyCollapsedHeightVal }
+    }
     
-    /// The maximum height of the header (fully expanded header)
-    let maxHeight: CGFloat = 500.0
-
-    /// how far the header view gets scrolled offscreen
-    var maxScrollAmount: CGFloat { maxHeight - minHeight }
+    /// The CGFloat value that controls the top elevator lift constraint of the HeaderView
+    @IBOutlet weak var fullyExpandedHeightConstraint: NSLayoutConstraint!
+    private(set) var fullyExpandedHeightVal: CGFloat = 0.0 {
+        didSet { fullyExpandedHeightConstraint.constant = fullyExpandedHeightVal }
+    }
+    
+    /// The CGFloat value that controls the top elevator lift constraint of the HeaderView
+    private(set) var headerTopLiftVal: CGFloat = 0.0 {
+        didSet { headerTopLiftConstraint.constant = headerTopLiftVal }
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
         
-        // adjust the scroll view's top inset to account for scrolling the header offscreen
-        tableView.contentInset = UIEdgeInsets(top: maxScrollAmount, left: 0.0, bottom: 0.0, right: 0.0)
+        updateHeaderHeights()
+        tableView.setContentOffset(.init(x: 0.0, y: 0.0), animated: false)
     }
     
-    override func viewDidAppear(_ animated: Bool) {
-        super.viewDidAppear(animated)
+    func updateHeaderHeights(_ minHeight: CGFloat? = nil, _ maxHeight: CGFloat? = nil) {
+        if let val = minHeight { fullyCollapsedHeightVal = val }
+        if let val = maxHeight { fullyExpandedHeightVal = val }
         
-        DispatchQueue.main.asyncAfter(deadline: .now()+2.0) { [self] in
-            print("\n\n\t\t\t setting tableView.contentOffset to \(tableView.contentInset.top.shortStr)\n\n")
-            tableView.setContentOffset(.init(x: 0.0, y: tableView.contentInset.top), animated: true)
-            
-            headerViewTop.constant = 0.0
-            view.layoutIfNeeded()
-            
+        if fullyCollapsedHeightVal + fullyExpandedHeightVal == 0.0 {
+            fullyCollapsedHeightVal = fullyCollapsedHeightConstraint.constant
+            fullyExpandedHeightVal  = fullyExpandedHeightConstraint.constant
         }
+        
+        // adjust the scroll view's top inset to account for scrolling the header offscreen
+        tableView.contentInset = .init(top: fullyExpandedHeightVal, left: 0.0, bottom: 0.0, right: 0.0)
     }
+    
+    private var maxScrollVal: CGFloat { fullyCollapsedHeightVal - fullyExpandedHeightVal }
 }
 
 // MARK:- ScrollViewContaining Delegate
@@ -54,17 +65,16 @@ extension ContainingViewController: UIScrollViewDelegate {
     
     func scrollViewDidScroll(_ scrollView: UIScrollView) {
         
-        print("\t\t scrollViewDidScroll began(A) - cont.off.y: \(scrollView.contentOffset.y.shortStr),\t cont.ins.top: \(scrollView.contentInset.top.shortStr),\t\t headerViewTop.con: \(headerViewTop.constant.shortStr) ")
+        let newHeightVal: CGFloat = maxScrollVal - scrollView.contentOffset.y
+        headerTopLiftVal = min(0.0, max(maxScrollVal, newHeightVal))
         
-        // need to adjust the content offset to account for the content inset
-        // negative because we are moving the header offscreen
-        let newTopConstraintConstant = -(scrollView.contentOffset.y + scrollView.contentInset.top)
-        headerViewTop.constant = min(0, max(-maxScrollAmount, newTopConstraintConstant))
+        let insetTopVal: CGFloat = min(max(0.0, -scrollView.contentOffset.y), fullyExpandedHeightVal - fullyCollapsedHeightVal)
+        scrollView.contentInset = .init(top: insetTopVal, left: 0.0, bottom: 0.0, right: 0.0)
         
-        print("\t\t scrollViewDidScroll ended(B) - cont.off.y: \(scrollView.contentOffset.y.shortStr),\t cont.ins.top: \(scrollView.contentInset.top.shortStr),\t\t headerViewTop.con: \(headerViewTop.constant.shortStr) ")
+        print("\t\t scrollViewDidScroll - cont.off.y: \(scrollView.contentOffset.y.shortStr),\t inset.top: \(scrollView.contentInset.top.shortStr),\t headerTop.con: \(headerTopLiftVal.shortStr)")
 
-        // handle color changes for collapsed state, check if scrollView is at the top
-        headerView.backgroundColor = headerViewTop.constant == -maxScrollAmount ? .green : .systemIndigo
+        // Changes color of header depending on collapse state
+        headerView.backgroundColor = headerTopLiftVal == maxScrollVal ? .orange : .systemIndigo
     }
 }
 
@@ -77,15 +87,17 @@ extension ContainingViewController: UITableViewDelegate, UITableViewDataSource {
     func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat { 0.0 }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "tableViewCell", for: indexPath) as! TVC
-        cell.titleLabel.text = "\(indexPath.row)"
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        var content = cell.defaultContentConfiguration()
+        content.text = "\(indexPath.row)"
+        cell.contentConfiguration = content
         return cell
     }
 
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat { UITableView.automaticDimension }
-}
-
-// MARK:- TableView Cell
-class TVC: UITableViewCell {
-    @IBOutlet weak var titleLabel: UILabel!
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: false)
+        
+    }
 }
